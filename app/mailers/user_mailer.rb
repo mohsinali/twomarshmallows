6ed86d4205cb828e6e5857633d0380d1d58a82f1
tahir-s <abumutahira@gmail.com>
2019-08-30 18:49:34 +0500

    mailer to instance variable

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,14 +2,14 @@ class UserMailer < ApplicationMailer
 
   def new_signup user
     @user             = user
-    @teacher_profile  = user.profile
+    @teacher_profile  = @user.profile
 
     mail(to: Settings.admin_email, subject: "A Teacher Showed Up")
   end
   
   def new_teacher_notify teacher
     @teacher  = teacher
-    @profile  = teacher.profile
+    @profile  = @teacher.profile
 
     mail(to: @teacher.email, subject: "TwoMarsmallow: Just A Little Wait") 
   end
