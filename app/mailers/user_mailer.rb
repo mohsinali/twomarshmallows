f19ec74d5934a204346b94478bc481f5e83e2a83
tahir-s <abumutahira@gmail.com>
2019-08-30 17:51:57 +0500

    teacher's admin approval email text change

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,9 @@
 class UserMailer < ApplicationMailer  
 
   def new_signup user
-    @user     = user
+    @user             = user
+    @teacher_profile  = user.teacher_profile
 
-    mail(to: Settings.admin_email, subject: "New Signup")
+    mail(to: Settings.admin_email, subject: "A Teacher Showed Up")
   end
 end
