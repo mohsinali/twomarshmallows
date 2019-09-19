9e6d94d4cfceac8ae090cfc27b31f156eba74a42
Mohsin <mohammadmohsin@gmail.com>
2019-09-19 08:40:08 +0500

    Logo and branding

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,12 +2,14 @@ class ProfileController < ApplicationController
   before_action :authenticate_user!
 
   def toggle_activate
-    @user = User.find_by(id: params[:id])    
+    @user           = User.find_by(id: params[:id])    
     @user.is_active ^= true
+    password        = Devise.friendly_token.first(6)
+    @user.password  = password
     @user.save
 
     ## Notify teacher when his account is activated.
-    TeacherMailer.account_activated(@user, Devise.friendly_token.first(6)).deliver_now if @user.is_active
+    TeacherMailer.account_activated(@user, password).deliver_now if @user.is_active
 
     respond_to do |format|
       format.js
