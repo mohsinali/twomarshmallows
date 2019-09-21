4e66a70c2d6e2ffe35f15082c768973174a9d96b
Mohsin <mohammadmohsin@gmail.com>
2019-09-21 19:31:45 +0500

    Setting host for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
     raise_delivery_errors: true
   }
 
-  config.action_mailer.default_url_options = { :host => 'americamp.hostrayatech.com' }
+  config.action_mailer.default_url_options = { :host => 'marshmallow.attribes.com' }
   config.action_mailer.raise_delivery_errors = true
   
   config.action_mailer.perform_deliveries = true
