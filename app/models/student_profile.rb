ab855206f436327763f39f00160c0a4a8c060f82
Mohsin <mohammadmohsin@gmail.com>
2019-09-21 11:29:31 +0500

    Overriding full_name field

--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -2,4 +2,8 @@ class StudentProfile < ApplicationRecord
   ## Associations
   belongs_to :user
   belongs_to :teacher, class_name: "User", foreign_key: "teacher_id"
+
+  def full_name
+    name
+  end
 end
